9ee36b2064e65def6ace42dd8d3ae18d4c61e718
Styne001 <47569127+Styne001@users.noreply.github.com>
2019-11-28 07:37:17 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from random import randint
 
+
 class PasswordGenerator:
 
-    #string of upper characters
+    # string of upper characters
     upper_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    
-    #string of lower characters
+
+    # string of lower characters
     lower_characters = 'abcdefghijklmnopqrstuvwxyz'
-    
-    #string of numbers
+
+    # string of numbers
     numbers = '0123456789'
 
-    #charset
+    # charset
     charset = None
 
     charset_constants = {
         'UPPERS_ONLY': 1,
-        'LOWERS_ONLY':2,
+        'LOWERS_ONLY': 2,
         'LOWERS_UPPERS': 3,
-        'NUMBERS_ONLY':4,
-        'UPPERS_NUMBERS':5,
-        'LOWERS_NUMBERS':6,
-        'UPPERS_LOWERS_NUMBERS':7
+        'NUMBERS_ONLY': 4,
+        'UPPERS_NUMBERS': 5,
+        'LOWERS_NUMBERS': 6,
+        'UPPERS_LOWERS_NUMBERS': 7
     }
-
 
     def set_charset(self, charset):
         if charset == 1:
@@ -42,11 +42,13 @@ class PasswordGenerator:
             self.charset == None
             print('Invalid charset selected')
 
-
     def generate_password(self):
         password = ""
-        for x in range(10):
-            x = randint(0, self.charset.__len__())
-            password += self.charset[x]
-
+        try:
+            length = int(input("Enter password length: "))
+            for x in range(length):
+                x = randint(0, self.charset.__len__())
+                password += self.charset[x]
+        except ValueError:
+            print("Enter whole number")
         return password
