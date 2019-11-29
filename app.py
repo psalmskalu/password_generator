dd6f68ee7d91d08cbad5b90abb5ec1f664edaebc
Styne001 <47569127+Styne001@users.noreply.github.com>
2019-11-29 10:13:00 +0100

    password length made flexible
    The password length which was fixed to be 10 is now flexible, again the error 'index out of bound is fixed' .

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class PasswordGenerator:
             try:
                 length = int((input("Enter password length: ")))
                 for x in range(length):
-                    x = randint(0, self.charset.__len__())
+                    x = randint(0, (self.charset.__len__() - 1))
                     password += self.charset[x]
                 return password
             except ValueError:
