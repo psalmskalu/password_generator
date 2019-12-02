cbeea347f0dbc9cbf846118ddea75f464a96af79
Styne001 <47569127+Styne001@users.noreply.github.com>
2019-12-02 11:23:44 +0100

    Modified set_charset function

--- app.py
+++ app.py
@@ -3,46 +3,8 @@ from random import randint
 
 class PasswordGenerator:
 
-    # string of upper characters
-    upper_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-    # string of lower characters
-    lower_characters = 'abcdefghijklmnopqrstuvwxyz'
-
-    # string of numbers
-    numbers = '0123456789'
-
-    # charset
-    charset = None
-
-    charset_constants = {
-        'UPPERS_ONLY': 1,
-        'LOWERS_ONLY': 2,
-        'LOWERS_UPPERS': 3,
-        'NUMBERS_ONLY': 4,
-        'UPPERS_NUMBERS': 5,
-        'LOWERS_NUMBERS': 6,
-        'UPPERS_LOWERS_NUMBERS': 7
-    }
-
-    def set_charset(self, charset):
-        if charset == 1:
-            self.charset = self.upper_characters
-        elif charset == 2:
-            self.charset = self.lower_characters
-        elif charset == 3:
-            self.charset = self.upper_characters + self.lower_characters
-        elif charset == 4:
-            self.charset = self.numbers
-        elif charset == 5:
-            self.charset = self.upper_characters + self.numbers
-        elif charset == 6:
-            self.charset = self.lower_characters + self.numbers
-        elif charset == 7:
-            self.charset = self.lower_characters + self.upper_characters + self.numbers
-        else:
-            self.charset is None
-            print('Invalid charset selected')
+    def set_charset(self, *args):
+        self.charset = ''.join(str(string) for string in args)
 
     def generate_password(self):
         password = ""
