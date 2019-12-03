2a207951c5f4ee11c273aff0797ffcff87e8c9fa
Styne001 <47569127+Styne001@users.noreply.github.com>
2019-12-03 09:22:02 +0100

    Modified to allow the use of special characters

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from app import PasswordGenerator
 
+# string of upper characters
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# string of lower characters
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+
+# string of numbers
+digits = '0123456789'
+
+# string of special characters
+specials = " !|\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
 pg = PasswordGenerator()
-pg.set_charset(7)
+pg.set_charset(specials, digits, lowercase, uppercase)
 print(pg.generate_password())
-
-
