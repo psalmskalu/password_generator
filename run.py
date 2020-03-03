dcdb3788955781bacd93f770aa89bf21809a2d8b
= <=>
2020-03-03 17:51:32 +0100

    Added the set_charset_length module

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import PasswordGenerator, Case
 
 pg = PasswordGenerator()
-pg.set_charset(Case.LOWERS_NUMBERS)
+pg.set_charset_length(55)
+pg.set_charset(Case.UPPERS_LOWERS_NUMBERS)
 
 #print the password generator
 print(pg.generate_password())
